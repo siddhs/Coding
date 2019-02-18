922cbde65c5e846c6d4e3d1308886f16f28289c7
Siddharth Sinha <siddharthsinha@Siddharths-MBP.fios-router.home>
2019-02-17 21:19:26 -0500

    LeetCode/Level-Easy/2 Approaches

---
+++ TwoSum.py
@@ -0,0 +1,31 @@
+def bruteForce(nums, target):
+	
+	# Time Complexity O(n^2)
+	i = 0
+	result = []
+	while i < (len(nums)-1):
+		j = i+1
+		while j < len(nums):
+			if (nums[i]+nums[j] == target):
+				result.append(i)
+				result.append(j)
+				return result
+			j += 1
+		i += 1
+
+def twoPassHash(nums, target):
+	# Two Pass Hash
+	# Time Complexity O(n)
+	# Space Complexity O(n)
+	dict = {}
+	for i in range(len(nums)):
+		dict[nums[i]] = i
+	for i in range(len(nums)):
+		complement = target-nums[i]
+		if complement in dict and i != dict[complement]:
+			return [i, dict[complement]]
+
+
+list = [3,3]
+print(bruteForce(list,6))
+print(twoPassHash(list,6))
